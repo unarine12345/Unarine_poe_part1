246255564658d8be063901f88e49885767026825
Unarine Hadzhi <ST10479464@rcconnect.edu.za>
2026-04-13 16:03:02 +0200

    i added a class called a askingf name which asks the name and displaywelcomemessage and check_name

---
+++ Unarine_poe_part1/prompt_user.cs
@@ -0,0 +1,142 @@
+﻿using System;
+
+namespace Unarine_poe_part1
+{
+    public class prompt_user
+    {//start of class
+
+        //global variable declaration, string datatype
+        //and variable name called name
+        //must be private
+        private string name = string.Empty;
+
+
+
+        //welcome the user
+        public void DisplayWelcomeMessage()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("======================--------------------------------========================");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("     [ Cybersecurity Awareness Bot – Stay Safe Online Always ]     ");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("======================--------------------------------========================");
+
+            Console.ResetColor();
+        }
+
+
+        //void method to prompt the user for name
+        //start with access modifier public
+        //then type of method void, then name of the
+        //method called asking_name()
+        public void asking_name()
+        { //start of asking name method
+
+            //ask for name
+
+            //AI and colors
+
+            //AI name
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("chatBot : ");
+
+            //AI message
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Please enter your name...");
+
+            //reset color
+            Console.ResetColor();
+
+            //do while to check and re-prompt the user
+            do
+            { //start of do while
+
+                //user entering name
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write("user : ");
+
+                //input color
+                Console.ForegroundColor = ConsoleColor.Gray;
+
+                name = Console.ReadLine();
+
+                //reset color
+                Console.ResetColor();
+
+
+            } while (!check_name());//end of do while
+
+
+        }//end of the asking name method
+
+        //boolean method to check if the user entered name
+        //start by the access modifier must be private
+        //then the type of method boolean then name of it
+        //check_name()
+        private Boolean check_name()
+        { //start of check_name
+
+
+            //check if the name is entered using if statement
+            if (name == "")
+            { //start if statement
+
+                //show error message
+                //AI name
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("chatBot : ");
+
+                //AI message
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Please try to enter your name...");
+
+                //reset color
+                Console.ResetColor();
+
+                //return false
+                return false;
+            }//end of if statement
+            else
+            {//start of else
+
+                //return the success message
+                //AI name
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("chatBot : ");
+
+                //AI message
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.Write("Hey ");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write(" " + name);
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine(" how can i help you");
+
+                //reset color
+                Console.ResetColor();
+                //return
+                return true;
+
+            }//end of else
+
+
+
+
+        }
+        //method to return user name
+        public string return_name()
+        {//start of method
+
+            //return the name of the user
+            return name;
+
+        } //end of metho
+    } //end of class
+
+
+
+}  //end of name space
+
