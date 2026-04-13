a2ee5cb801b3103a594b8778155637dd098db2d9
Unarine Hadzhi <ST10479464@rcconnect.edu.za>
2026-04-13 15:38:35 +0200

    i created a class called program which contains instance which are called prompt_user collect_name and chats and command logo

--- Unarine_poe_part1/Program.cs
+++ Unarine_poe_part1/Program.cs
@@ -6,10 +6,36 @@ using System.Threading.Tasks;
 
 namespace Unarine_poe_part1
 {
-    internal class Program
-    {
+    public class Program
+    {//start of class
+
         static void Main(string[] args)
-        {
-        }
-    }
-}
+        {//start of main method
+
+            //creating instance for the class voice_logo
+            //without an object name but a constructor
+            new command_logo() { };
+            //creating an instance for the class prompt_user
+            //with an object name collect_name
+            prompt_user collect_name = new prompt_user();
+            //calling welcome method
+            collect_name.DisplayWelcomeMessage();
+            collect_name.asking_name();
+
+            //user and ai chat
+            //instance for the class ai_response
+            //with an object name chatting chatting
+            chats chatting = new chats();
+
+            //get the return name of user
+            String name = collect_name.return_name();
+            //now starting the chat
+            chatting.ai_chats(name);
+
+
+
+        }//end of main method
+
+    }// end of class
+
+}//end of name space
---
+++ Unarine_poe_part1/command_logo.cs
@@ -0,0 +1,9 @@
+﻿namespace Unarine_poe_part1
+{
+    internal class command_logo
+    {
+        public command_logo()
+        {
+        }
+    }
+}
